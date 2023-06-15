f5745a52992a2c31e0cc08fe638c0acd92189486
Tonya Fletcher <tfletch4504@gmail.com>
2023-06-15 14:43:59 -0400

    move styling for media player to .css

--- my-music/src/components/Media/index.js
+++ my-music/src/components/Media/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import prelude1 from "../../images/prelude1.mp4";
-import test from "../../images/test.jpeg";
 import "./style.css";
 
 
@@ -9,14 +8,11 @@ const Player = () => {
     return (
         <div>
             <video 
-            width="50%"
-             height="50%" 
              controls>
                 <source 
                 src={prelude1}
                 type="video/mp4"
-                poster={test}
-        primaryColor="var(--amethystOrchid)" />
+                />
             </video>
         </div>
     )
