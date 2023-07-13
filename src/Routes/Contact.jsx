94c199f05ce459ada77e4b12c11d4068a379a571
Tonya Fletcher <tfletch4504@gmail.com>
2023-07-13 11:48:10 -0400

    fab styling

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Container from "../components/Container";
 import Row from "../components/Row";
+import social from "../profile.js"
 import "../index.css";
 
 function Contact() {
@@ -8,15 +9,26 @@ function Contact() {
         <div className="">
            
 
-                <Container>
+                <Container fluid style={{ marginBottom: 100 }}>
                     <Row>
                     <h1 className="home">letsLinkUp</h1>
                     </Row>
                 </Container>
 
                 <Container>
-                <p className="text-center">Something Here</p>
+                {/* <p className="text-center">Something Here</p> */}
+                <div className="d-flex justify-content-center align-items-center flex-column">
 
+                <div className="fabs inline-block">
+                    {social.linkedin && <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank"  href={social.linkedin}><i className="fab fa-linkedin"></i></a>}
+                    {social.facebook && <a title="Visit Facebok profile" rel="noopener noreferrer" target="_blank" href={social.facebook}><i className="fab fa-facebook"></i></a>}
+                    {social.instagram && <a title="Visit Instagram profile" rel="noopener noreferrer" target="_blank" href={social.instagram}><i className="fab fa-instagram"></i></a>}
+                    {social.github && <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}><i className="fab fa-github"></i></a>}<br/>
+                </div>
+                <div>
+                    {social.resume && <a title="Download Resume" href={social.resume} download><i className="fas fa-download"></i></a>}
+                </div>
+                </div>
                 </Container>
 
           
---
+++ src/profile.js
@@ -0,0 +1,9 @@
+const social = {
+    github: "https://github.com/tfletch3018",
+    facebook: "https://www.facebook.com/tonya.r.fletcher/",
+    linkedin: "https://www.linkedin.com/in/tonya-fletcher-733a9450/",
+    instagram: "https://www.instagram.com/tfletch4504/",
+    resume: "https://drive.google.com/file/d/1amGP50-pMBJRF0_ZZEQAkJNeQLSaFH3n/view?usp=drive_link",
+  };
+
+  export default social;
