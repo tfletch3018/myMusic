fbd929b17843339652ea248ee5c1567a829201b8
Tonya Fletcher <tfletch4504@gmail.com>
2023-08-11 19:00:49 -0400

    add tiktok to social

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -19,7 +19,8 @@ function Contact() {
                         {social.linkedin && <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank" href={social.linkedin}><i className="fab fa-linkedin"></i></a>}
                         {social.facebook && <a title="Visit Facebok profile" rel="noopener noreferrer" target="_blank" href={social.facebook}><i className="fab fa-facebook"></i></a>}
                         {social.instagram && <a title="Visit Instagram profile" rel="noopener noreferrer" target="_blank" href={social.instagram}><i className="fab fa-instagram"></i></a>}
-                        {social.github && <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}><i className="fab fa-github"></i></a>}<br />
+                        {social.github && <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}><i className="fab fa-github"></i></a>}
+                        {social.tiktok && <a title="Visit Instagram profile" rel="noopener noreferrer" target="_blank" href={social.tiktok}><i className="fab fa-tiktok"></i></a>}<br/>
                     </div>
                     <div>
                         {social.resume && <a title="Download Resume" href={social.resume} download><i className="fas fa-download"></i></a>}
