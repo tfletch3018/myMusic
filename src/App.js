2ae20b846dbc3aa2bd6b191406395d8a2d65a311
Tonya Fletcher <tfletch4504@gmail.com>
2023-08-28 18:57:26 -0400

    add logo to navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Outlet, Link } from "react-router-dom";
+import myMusicPic from "../src/images/myMusicPic.ico";
 import "./index.css";
 
 
@@ -6,15 +7,15 @@ import "./index.css";
 function App() {
   return (
 
-
     <div>
 
       <nav>
-        <Link to="/home">myMusic</Link> {" "}
+        <Link to="/"><img src={myMusicPic} id="logo" alt="" /></Link> {" "}
+        <Link to="/home">myMusic</Link>
         <Link to="/videos">myVideos</Link>
         <Link to="/contact">letsLinkUp</Link>
-
       </nav>
+
       <Outlet />
     </div>
   );
