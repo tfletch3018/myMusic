421668941dd9079f1f0162de9e76b2990a0c5874
Tonya Fletcher <tfletch4504@gmail.com>
2025-04-01 17:29:16 -0400

    add media screen queries to Tales

--- src/Routes/Tales.jsx
+++ src/Routes/Tales.jsx
@@ -1,6 +1,5 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import { Link } from "react-router-dom";
 import bookCoverPage1 from "../images/bookCoverPage1.jpg";
 import social from "../profile.js";
@@ -13,31 +12,46 @@ function Tales() {
             <Container fluid style={{ marginTop: 50 }}>
 
                 <Container>
-                    {/* <Row> */}
                     <div className="center">
                         <Link to="/home">
                             <button className="modalBtn">Go Back</button>
                         </Link>
                     </div>
-                    {/* </Row> */}
                 </Container>
 
                 <Container fluid style={{ marginTop: 50 }}>
-                    <Row className='tales'>
-                        <Col><img className="shimmer" src={bookCoverPage1} alt="" /></Col>
-                        <Col>
+
+                    <div className='myCover'>
+
+                        <div className=''>
+                            <img id="scaled"
+                                className="shimmer"
+                                src={bookCoverPage1}
+                                alt="" />
+                        </div>
+
+                        <div className=''>
                             <p className='description'>
-                                A coming of age tale depicting young love. Finding it, fighting for it, losing it, and learning from it all. 
-                                Follow Chloe as she experiences important life lessons about love, relationships, and coping with loss.
+                                A coming of age tale depicting young love. Finding it, fighting for it, losing it,
+                                and learning from it all. Follow Chloe as she experiences important life lessons
+                                about love, relationships, and coping with loss.
                             </p>
                             <p className='description'>
-                                A story that is dedicated to anyone who has ever felt a sense of isolation...a reminder that you are not alone.
-                                While the circumstances of our strifes may shift and evolve, it is only human arrogance that convices us that we are "the first" or "the only one."
-                                The truth is, human beings have been experiencing the vast array of states of being from exuberance to utter despair and adapting to it all long before any of us could take a breath.
+                                A story that is dedicated to anyone who has ever felt a sense of isolation...
+                                a reminder that you are not alone. While the circumstances of our strifes may
+                                shift and evolve, it is only human arrogance that convices us that we are "the first"
+                                or "the only one." The truth is, human beings have been experiencing the vast array
+                                of states of being from exuberance to utter despair and adapting to it all long before
+                                any of us could take a breath.
                             </p>
-                            <p className='description'>Chloe is humbled as she learns that "this too shall pass" and finds comfort and strength in the knowlege that each one of us has her own tale as old as time.</p>
-                        </Col>
-                    </Row>
+                            <p className='description'>
+                                Chloe is humbled as she learns that "this too shall pass" and finds comfort and strength
+                                in the knowlege that each one of us has her own tale as old as time.
+                            </p>
+
+                        </div>
+                    </div>
+
                 </Container>
 
                 <Container fluid style={{ marginTop: 50, marginBottom: 50, }}>
